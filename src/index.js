f44e25e808c6cededb18f215429242acea06ac3a
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-09-08 16:01:39 -0300

    Installed Some Dependencies

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { View, Text } from 'react-native';
+
+export default function Index(){
+    return(
+        <NavigationContainer>
+
+            <View>
+
+                
+
+            </View>
+
+        </NavigationContainer>
+    )
+}
