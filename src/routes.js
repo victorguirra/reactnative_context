63293e954c34f54411568928683eff98d23eb337
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-09-08 15:13:13 -0300

    Creating Routes File and Importing in App

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { View } from 'react-native';
+
+export default function Routes(){
+    return(
+        <NavigationContainer>
+            <View></View>
+        </NavigationContainer>
+    )
+}
