792a5ba0ccd16a5c8f39dba6f237328fd14ceb24
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-09-08 15:34:12 -0300

    Send Initial Files

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-
-import Routes from './src/routes';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Routes />
-    </
-    >
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
