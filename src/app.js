fa4c97630be906b156d69b2e93d6316bff710d2c
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-09-08 16:49:23 -0300

    Moved App for Src Folder

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { View, Text } from 'react-native';
+
+export default function App(){
+    return(
+        <NavigationContainer>
+
+            <View>
+
+                <Text>Here Go App</Text>
+
+            </View>
+
+        </NavigationContainer>
+    )
+}
