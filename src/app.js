38312329ce405960894728f5136ef4616d96ce8d
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-09-09 11:01:50 -0300

    Created Button in SignIn Screen

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 
 const App = () => {
     return(
         <NavigationContainer>
 
             <View>
-
-                <Text>Here Go App</Text>
 
             </View>
 
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Button } from 'react-native';
 
 const SignIn = () => {
     return(
         <View>
 
-
+            <Button title="SignIn" onPress={ () => {} } />
 
         </View>
 
