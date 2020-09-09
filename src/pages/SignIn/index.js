7060142744acdc4dd3bbc1ce0c1f50fb698a4d7d
Victor Guirra <victor.hugo.guirra@gmail.com>
2020-09-09 12:11:53 -0300

    Configured User Data

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, StyleSheet } from 'react-native';
+
+import { signIn } from '../../services/auth';
+
+const styles = StyleSheet.create({
+    container: { flex:1, justifyContent:'center' }
+});
 
 const SignIn = () => {
-    return(
-        <View>
+    async function handleSignIn(){
+        const response = await signIn();
 
-            <Button title="SignIn" onPress={ () => {} } />
+        console.log(response);
+    }
+    
+    return(
+        <View style={ styles.container } >
+
+            <Button title="SignIn" onPress={ handleSignIn } />
 
         </View>
 
